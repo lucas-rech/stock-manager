d75551d505c5e4d04079bfd54d7154f38b0951b6
Lucas Rech <lucasrech00@gmail.com>
2025-08-03 12:42:27 -0300

    config: add dotenv

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import { stockRouter } from './router/stock_router.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
